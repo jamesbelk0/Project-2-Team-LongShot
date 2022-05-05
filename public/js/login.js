5a258c22062e91004a54312a1e91b85eba073cd6
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-05-04 20:15:55 -0400

    added signup functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,27 @@
 //(PLACEHOLDER) function to gather form data and call our "POST /api/user/login" express route
 const loginFormHandler = async function (event) {
   event.preventDefault();
+  
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    // check the response status
+    if (response.ok) {
+      console.log('success');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
 
 };
