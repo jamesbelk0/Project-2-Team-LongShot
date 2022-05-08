ee9ce756f0e33c940864bb8fb38c8f816f4bff65
Cole <deoncole77@gmail.com>
2022-05-08 13:30:47 -0400

    add GET to User route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//GET to find all users
+router.get('/', (req,res) => {
+  User.findAll({
+
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 //POST /api/user/
 router.post('/', (req, res) => {
   User.create({
