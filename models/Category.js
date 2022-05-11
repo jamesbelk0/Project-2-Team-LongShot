a67203598f34cfb88b64b6d1ac82a9235949f05a
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-05-10 20:20:38 -0400

    working template. thank the lord almighty

--- models/Category.js
+++ models/Category.js
@@ -1,36 +1,36 @@
-// require the model and datatypes form sequelize
-const { Model, DataTypes } = require('sequelize');
-// require the connection
-const sequelize = require('../config/config');
+// // require the model and datatypes form sequelize
+// const { Model, DataTypes } = require('sequelize');
+// // require the connection
+// const sequelize = require('../config/config');
 
-class Category extends Model {}
+// class Category extends Model {}
 
-Category.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        category_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        category_id: {
-            type: DataTypes.id,
-            references: {
-                model: 'post',
-                key: 'id'
-            }
-        }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'category'
-    }
-);
+// Category.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
+//         category_name: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         },
+//         category_id: {
+//             type: DataTypes.id,
+//             references: {
+//                 model: 'post',
+//                 key: 'id'
+//             }
+//         }
+//     },
+//     {
+//         sequelize,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'category'
+//     }
+// );
 
-module.exports = Category;
+// module.exports = Category;
