2554531505b454cd318b0d6156277466738b7d45
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-05-10 20:04:01 -0400

    potentially working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+const categoryRoutes = require('./category-routes')
+
 
 router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
+router.use('/category', categoryRoutes);
 
 module.exports = router;
