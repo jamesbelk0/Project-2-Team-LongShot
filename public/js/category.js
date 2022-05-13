ed864ea2e393e10895d9449e2459849ff3aa701c
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-05-13 16:47:53 -0400

    added event listeners to each category

--- public/js/category.js
+++ public/js/category.js
@@ -1,1 +1,19 @@
-const automotive  = document.querySelector('#automotive')
+const automotive  = document.getElementById('automotive')
+automotive.addEventListener('click', submitBtnHandler);
+
+const health  = document.getElementById('health')
+health.addEventListener('click', submitBtnHandler);
+
+const food  = document.getElementById('food')
+food.addEventListener('click', submitBtnHandler);
+
+const pets  = document.getElementById('pets')
+pets.addEventListener('click', submitBtnHandler);
+
+const homeImprovement  = document.getElementById('home-improvement')
+homeImprovement.addEventListener('click', submitBtnHandler);
+
+const diy  = document.getElementById('diy')
+diy.addEventListener('click', submitBtnHandler);
+
+
