6e63ec204ec5ccba63c71f1975e2c6bee3851b4a
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-05-13 16:54:05 -0400

    added javascript functionality to all the category buttons

--- public/js/category.js
+++ public/js/category.js
@@ -1,19 +1,43 @@
 const automotive  = document.getElementById('automotive')
-automotive.addEventListener('click', submitBtnHandler);
+automotive.addEventListener('click', viewAutomotive);
 
 const health  = document.getElementById('health')
-health.addEventListener('click', submitBtnHandler);
+health.addEventListener('click', viewHealth);
 
 const food  = document.getElementById('food')
-food.addEventListener('click', submitBtnHandler);
+food.addEventListener('click', viewFood);
 
 const pets  = document.getElementById('pets')
-pets.addEventListener('click', submitBtnHandler);
+pets.addEventListener('click', viewPets);
 
 const homeImprovement  = document.getElementById('home-improvement')
-homeImprovement.addEventListener('click', submitBtnHandler);
+homeImprovement.addEventListener('click', viewHomeImprovement);
 
 const diy  = document.getElementById('diy')
-diy.addEventListener('click', submitBtnHandler);
+diy.addEventListener('click', viewDiy);
 
 
+async function viewAutomotive() {
+    // render automotive posts
+    console.log('This worked!')
+};
+
+async function viewHealth() {
+    // render health posts
+};
+
+async function viewFood() {
+    // render food posts
+};
+
+async function viewPets() {
+    // render pet posts
+};
+
+async function viewHomeImprovement() {
+    // render home improvement posts
+};
+
+async function viewDiy() {
+    // render diy posts
+};
