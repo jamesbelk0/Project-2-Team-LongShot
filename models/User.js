44cc366418a2c25c25be60771de8a829f4b7c9b9
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-05-16 13:45:41 -0400

    bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/config');
 
 // create our User model
