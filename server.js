6e58743a5a770fad0d15ee9c0c09b9820fc5f561
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-05-16 09:09:26 -0400

    tweaking server.js

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require('./utils/helpers');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 
 const sequelize = require("./config/config");
 const { start } = require("repl");
